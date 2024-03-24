7d5a29e05be5587505b752fbb68d8eee9b43f735
ThanhThat <thachthanhthat1995@gmail.com>
2024-03-25 00:31:12 +0700

    Lam anh 3 nguoi.

--- src/components/FormAddPhotoOfThreePeople.vue
+++ src/components/FormAddPhotoOfThreePeople.vue
@@ -16,14 +16,19 @@
     </div>
 
     <button type="submit" class="btn btn-primary">Tải ảnh lên</button>
+
+    <img src="/images/1.jpg" id="image1" v-show="false" />
+    <img src="/images/2.jpg" id="image2" v-show="false" />
+    <img :src="this.newImage" id="image3" v-show="false" />
   </form>
   <canvas
-    ref="canvas"
+    id="canvas"
     width="755.90551181"
     height="566.92913386"
     data-bs-toggle="tooltip"
     data-bs-placement="top"
     title="Click để tải ảnh"
+    @click="downloadImage"
   ></canvas>
 </template>
 
@@ -33,104 +38,52 @@ import { handleStudentName } from '../utilities'
 export default {
   data() {
     return {
-      images: [],
-      canvas: null,
-      context: null,
-      image: null
+      // image declaration
+      fixedImage: ['/images/1.jpg', '/images/2.jpg'],
+      newImage: '',
+      newImageName: '',
+      //canvas
+      context: ''
     }
   },
 
   mounted() {
-    this.canvas = this.$refs.canvas
-    this.context = this.canvas.getContext('2d')
-
-    this.canvas.addEventListener('click', this.downloadImages)
+    const canvas = document.getElementById('canvas')
+    console.log(canvas)
+    this.context = canvas.getContext('2d')
   },
 
   methods: {
     chooseImg(e) {
-      let files = e.target.files
+      const imageFile = e.target.files[0]
+      this.newImageName = handleStudentName(imageFile.name)
 
-      if (files.length <= 15) {
-        for (let i = 0; i < files.length; i++) {
-          const reader = new FileReader()
-          const name = handleStudentName(files[i].name)
-          reader.onload = (e) => {
-            this.addImage(e.target.result, name)
-          }
+      const theReader = new FileReader()
 
-          reader.readAsDataURL(files[i])
-        }
-      } else {
-        alert('Vui lòng không upload quá 15 ảnh!')
-        this.$refs['inputFile'].value = ''
+      theReader.onload = async () => {
+        this.newImage = await theReader.result
       }
+
+      theReader.readAsDataURL(imageFile)
     },
 
     submit() {
-      // this.$emit('addPhotoOfThreePeople', this.images)
-      // this.$refs['inputFile'].value = ''
-      // this.images = []
-
-      this.drawImages()
-
-      this.$refs['inputFile'].value = ''
-      // this.images = []
+      console.log(this.context)
+      const image1 = document.getElementById('image1')
+      const image2 = document.getElementById('image2')
+      const image3 = document.getElementById('image3')
+      this.context.drawImage(image1, 0, 0, 302.36220472, 566.92913386)
+      this.context.drawImage(image2, 302.36220472, 302.36220472, 226.77165354, 302.36220472)
+      this.context.drawImage(image3, 529, 302.36220472, 226.77165354, 302.36220472)
     },
 
-    addImage(src, name) {
-      const newImage = new Image()
-      this.images = []
-      newImage.onload = () => {
-        this.images.push({
-          src,
-          image: newImage,
-          width: 302.36220472,
-          height: 566.92913386,
-          name
-        })
-
-        this.images.push({
-          src,
-          image: newImage,
-          width: 226.77165354,
-          height: 302.36220472,
-          name
-        })
-
-        this.images.push({
-          src,
-          image: newImage,
-          width: 226.77165354,
-          height: 302.36220472,
-          name
-        })
-      }
-
-      newImage.src = src
-    },
-
-    drawImages() {
-      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
-      let x = 0
-
-      this.images.forEach((image, index) => {
-        if (index !== 0) {
-          this.context.drawImage(image.image, x, 302.36220472, image.width, image.height)
-        } else {
-          this.context.drawImage(image.image, x, 0, image.width, image.height)
-        }
-
-        x += image.width // Tăng x để dịch chuyển đến vị trí cho ảnh tiếp theo
-      })
-    },
-
-    downloadImages() {
-      const dataURL = this.canvas.toDataURL('image/jpg')
+    downloadImage() {
+      const canvas = document.getElementById('canvas')
+      const dataURL = canvas.toDataURL('image/jpg')
       const link = document.createElement('a')
+
       link.href = dataURL
-      link.download = this.images[0].name + '.jpg'
-      document.body.appendChild(link)
+      link.download = this.newImageName + '.jpg'
       link.click()
       document.body.removeChild(link)
     }
